70cb8159dd9039eda6e5e6e982c94d28fb4ece01
koki-asami <is0392hr@gmail..com>
2023-05-05 22:20:51 +0900

    added function to revise slide and render

--- gpt.py
+++ gpt.py
@@ -14,30 +14,26 @@ text_splitter = RecursiveCharacterTextSplitter(
     length_function = len,
 )
 
-def process_pdf(pdf_path, revision_text=None):
+def process_pdf(pdf_path):
     loader = PyPDFLoader(pdf_path)
     pages = loader.load_and_split()
 
     state_of_the_union = "".join([x.page_content for x in pages])
     texts = text_splitter.create_documents([state_of_the_union])
 
-    os.environ["OPENAI_API_KEY"]="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"
-
     embeddings = OpenAIEmbeddings()
-    filename = "faiss_index"  # チェックするファイル名
+    filename = f"faiss_index/{pdf_path.split('/')[-1]}"  # チェックするファイル名
     if os.path.isdir(filename):
-        vectorstore = FAISS.load_local("faiss_index", embeddings)
+        vectorstore = FAISS.load_local(filename, embeddings)
     else:
         vectorstore = FAISS.from_documents(texts, embedding=embeddings)
-    vectorstore.save_local("faiss_index")
+    vectorstore.save_local(filename)
 
     llm = ChatOpenAI(temperature=0, model_name="gpt-3.5-turbo")
     retrieval_chain_agent = ConversationalRetrievalChain.from_llm(llm, vectorstore.as_retriever(), return_source_documents=True)
 
-    # topic = "CAMEL"
-
     query = f'''
-    重要なテーマを3つまでカンマ区切りで挙げてください。カンマ区切りのテーマのリストのみを出力してください。
+    この論文について重要なテーマを3つまでカンマ区切りで挙げてください。カンマ区切りの単語のリストのみを出力してください。
     '''
     chat_history = []
 
@@ -59,8 +55,19 @@ def process_pdf(pdf_path, revision_text=None):
         results.append(result["answer"])
 
     status = '''
-    あなたは優秀なエバンジェリストです。与えられた文章について簡潔に説明するためのスライドを作成します。
-    スライドを作成する際は次のようなMarpのデザインテンプレートを使用したマークダウン形式で表現されます。
+    あなたは熟練のコンサルタントです。与えられた文章について簡潔に説明するためのスライドを作成します。
+    最初に次のようなMarpのデザインテンプレートを使用したマークダウン形式でタイトルスライドを作成してください。
+    """
+
+    ---
+    <!-- スライド n -->
+    # { タイトル }
+
+    - { 作者名 }
+    - { 日付 }
+
+    """
+    次に、スライドの本文を作成する際は次のようなMarpのデザインテンプレートを使用したマークダウン形式で表現されます。
     箇条書きの文はできるだけ短くまとめてください。
     """
 
@@ -98,20 +105,26 @@ def process_pdf(pdf_path, revision_text=None):
         slides.append(answer)
 
     output_str = """---
-    marp: true
-    _theme: gaia
-    paginate: true
-    backgroundColor: #f5f5f5
+marp: true
+_theme: gaia
+paginate: true
+backgroundColor: #f5f5f5
 
     """
+
+    output_path = f'download/{(pdf_path.split("/")[-1]).split("/")[0]}_slide.md'
+    # with open(output_path, "r") as file:
+    #     test_output_str = file.readlines()
+    # output_str = "".join(test_output_str)
+    # print(output_str)
     for i in slides:
         output_str += i + "\n"
 
-    output_path = f'download/{(pdf_path.split("/")[-1]).split("/")[0]}_slide.md'
+    
     print(output_path)
     with open(output_path, "w") as file:
         file.write(output_str)
     
     os.system(f"npx @marp-team/marp-cli@latest {output_path} --pdf -y")
 
-    return output_path.replace(".md", ".pdf")
+    return (output_path.replace(".md", ".pdf"), output_str)
--- main.py
+++ main.py
@@ -1,8 +1,17 @@
-from fastapi import FastAPI, File, UploadFile
+from typing import List
+from fastapi import FastAPI, File, UploadFile, WebSocket, WebSocketDisconnect
 from fastapi.responses import FileResponse
 from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 from gpt import process_pdf
+import json
+import os
+import re
+import openai
+import asyncio
+
+os.environ["OPENAI_API_KEY"]=""
+openai.api_key = ""
 
 app = FastAPI()
 # CORSを回避するための設定(ホストが違ってもアクセスを許可する)
@@ -14,6 +23,27 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+
+class ConnectionManager:
+    def __init__(self):
+        self.active_connections: List[WebSocket] = []
+
+    async def connect(self, websocket: WebSocket):
+        await websocket.accept()
+        self.active_connections.append(websocket)
+
+    def disconnect(self, websocket: WebSocket):
+        self.active_connections.remove(websocket)
+
+    async def send_personal_message(self, message: str, websocket: WebSocket):
+        await websocket.send_text(message)
+
+    async def broadcast(self, message: str):
+        for connection in self.active_connections:
+            await connection.send_text(message)
+
+manager = ConnectionManager()
+
 class RequestData(BaseModel):
     chattext: str
 
@@ -21,14 +51,123 @@ class RequestData(BaseModel):
 async def create_user(requestdata: RequestData):
     return {"chattext": requestdata.chattext}
 
-# ファイルをアップロードするAPI
-@app.post("/files/")
-async def create_file(file: UploadFile = File(...)):
-    file_data = await file.read()
-    # file_dataには、アップロードされたPDFファイルのバイナリデータが含まれます
-    file_path = f"upload/{file.filename}"
-    with open(file_path, "wb") as f:
-        f.write(file_data)
+@app.get("/files/")
+async def get_file(file_path:str):
     output_file = process_pdf(file_path)
     # return {"filename": file.filename}
     return FileResponse(output_file)
+
+# # ファイルをアップロードするAPI
+# @app.post("/files/")
+# async def create_file(file: UploadFile = File(...)):
+#     file_data = await file.read()
+#     # file_dataには、アップロードされたPDFファイルのバイナリデータが含まれます
+#     file_path = f"upload/{file.filename}"
+#     with open(file_path, "wb") as f:
+#         f.write(file_data)
+#     output_file = process_pdf(file_path)
+#     # return {"filename": file.filename}
+#     return FileResponse(output_file)
+
+
+@app.websocket("/ws/chattext/")
+async def websocket_endpoint(websocket: WebSocket):
+    await websocket.accept()
+    while True:
+        data = await websocket.receive_text()
+        await websocket.send_text(f"Message text was: {data}")
+
+
+@app.websocket("/ws/chat/")
+async def websocket_endpoint(websocket: WebSocket):
+    await websocket.accept()
+    system_prompt = '''
+    あなたは熟練のコンサルタントです。あなたが出力したスライドに応じて、ユーザが修正の要望を出すので、要望に合ったMarpで使えるMarkdown形式で修正した結果のみを出力してください。
+    Maprのフォーマットは以下の通りです。
+    """
+
+    ---
+    <!-- スライド n -->
+    # { タイトル }
+
+    - { 本文 }
+    - { 本文 }
+    - { 本文 } 
+
+    """
+    
+    スライドの修正が完了したら変更分や追加分だけでなく変更を行なっていない部分についても合わせて完全な形式で出力してください。
+    '''
+    messages = [{"role": "system", "content": system_prompt}]
+    while True:
+        json_string = await websocket.receive_text()
+        data = json.loads(json_string)
+
+        filename = data['filename']
+        message = data['message']
+        print(message)
+        if filename != "":
+            file = await websocket.receive_bytes() 
+            file_path = f"upload/{filename}"
+
+            with open(file_path, "wb") as f:
+                f.write(file)
+
+            output_file, output_message = process_pdf(file_path)
+            messages.append({"role": "assistant", "content": output_message})
+            with open(output_file, mode="rb") as file:
+            # WebSocketを使用してファイルのバイナリデータをストリーミング
+                while True:
+                    data = file.read(1024)
+                    if not data:
+                        break
+                    await websocket.send_bytes(data)
+            await websocket.send_text("file_receive_success")
+            await websocket.send_text(json.dumps({"state":"completed", "message":"How do you like it? If you need to modify slide including design, type here:"}))
+        
+        else:
+            # chatgptで手直し            
+            messages.append({"role": "user", "content": message})
+            response = openai.ChatCompletion.create(
+                model="gpt-3.5-turbo",
+                stream=True,
+                messages=messages
+            )
+            answer = []
+            for event in response:
+                event_text = event['choices'][0]['delta']
+                answer.append(event_text.get('content', ''))
+                if answer:
+                    print("".join(answer))
+                    await websocket.send_text(json.dumps({"state":"processing", "message":"".join(answer)}))
+                    await asyncio.sleep(0.01)
+            await websocket.send_text(json.dumps({"state":"completed", "message":"".join(answer)}))
+            messages.append({"role": "assistant", "content": "".join(answer)})
+
+            # output_slide_index = "".join(answer).find("---")
+            # output_slide = "".join(answer)[output_slide_index:]
+            pattern = r"```(.*?)```"
+            output_slide = re.findall(pattern, "".join(answer))
+            if len(output_slide):
+                output_slide = output_slide[0]
+            else:
+                output_slide = "".join(answer)
+            
+            output_slide_index = output_slide.find("---")
+            output_slide = output_slide[output_slide_index:]
+            output_path = f'download/{(file_path.split("/")[-1]).split("/")[0]}_slide.md'
+           
+            print(output_path)
+            with open(output_path, "w") as file:
+                file.write(output_slide)
+            
+            os.system(f"npx @marp-team/marp-cli@latest {output_path} --pdf -y")
+            
+            with open(output_file, mode="rb") as file:
+            # WebSocketを使用してファイルのバイナリデータをストリーミング
+                while True:
+                    data = file.read(1024)
+                    if not data:
+                        break
+                    await websocket.send_bytes(data)
+            await websocket.send_text("file_receive_success")
